b189e1e7a3c8cefa06d6421c48bf9da4ea707a7f
brxken128 <77554505+brxken128@users.noreply.github.com>
2022-05-02 19:48:21 +0100

    update to 1.9.2

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ mod structs;
 
 fn main() -> Result<()> {
     let matches = Command::new("dexios") // add verbose arg?
-        .version("1.9.1")
+        .version("1.9.2")
         .author("brxken128 <github.com/brxken128>")
         .about("Secure command-line encryption of files.")
         .subcommand_required(true)

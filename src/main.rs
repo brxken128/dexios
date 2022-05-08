30301a82f1e5e1bf0077839685b67c8e574de4a8
brxken128 <77554505+brxken128@users.noreply.github.com>
2022-05-08 14:09:19 +0100

    5.0.12

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ mod structs;
 
 fn main() -> Result<()> {
     let matches = Command::new("dexios")
-        .version("5.0.11")
+        .version("5.0.12")
         .author("brxken128 <github.com/brxken128>")
         .about("Secure command-line encryption of files.")
         .subcommand_required(true)

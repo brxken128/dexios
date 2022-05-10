d1d3acab69a96d0b1f5c4a540219c46a535e8a30
brxken128 <77554505+brxken128@users.noreply.github.com>
2022-05-10 11:51:54 +0100

    improve erase speed and add duration output

--- src/erase.rs
+++ src/erase.rs
@@ -2,17 +2,13 @@ use anyhow::{Context, Result};
 use rand::Rng;
 use std::{
     fs::File,
-    io::{BufReader, BufWriter, Read, Write},
+    io::{BufWriter, Write}, time::Instant,
 };
 
 pub fn secure_erase(input: &str) -> Result<()> {
+    let start_time = Instant::now();
     let file = File::open(input).context("Unable to open the input file")?;
-    let mut reader = BufReader::new(file);
-    let mut data = Vec::new(); // our file bytes
-    reader
-        .read_to_end(&mut data)
-        .context("Unable to read the input file")?;
-    drop(reader);
+    let data = file.metadata()?;
 
     for _ in 0..32 {
         // overwrite the data 16 times with random bytes
@@ -31,7 +27,7 @@ pub fn secure_erase(input: &str) -> Result<()> {
     }
 
     // overwrite with zeros for good measure
-    let zeros: Vec<u8> = vec![0; data.len()];
+    let zeros: Vec<u8> = vec![0; data.len().try_into().unwrap()];
     let file = File::create(input).context("Unable to open the input file")?;
     let mut writer = BufWriter::new(file);
     writer
@@ -46,6 +42,10 @@ pub fn secure_erase(input: &str) -> Result<()> {
     drop(file);
 
     std::fs::remove_file(input).context("Unable to remove file")?;
+
+    let duration = start_time.elapsed();
+
+    println!("Erased {} successfully [took {:.2}s]", input, duration.as_secs_f32());
 
     Ok(())
 }

ba1bf624f7bab987562c5b643026d89ddfd7d337
brxken128 <77554505+brxken128@users.noreply.github.com>
2022-05-12 15:57:01 +0100

    standardise hashing between both modes

--- src/hashing.rs
+++ src/hashing.rs
@@ -1,11 +1,12 @@
 use anyhow::{Ok, Result};
 
 use crate::structs::DexiosFile;
-use std::io::Write;
 
-pub fn hash_data_blake3(data: DexiosFile) -> Result<String> {
+pub fn hash_data_blake3(data: &DexiosFile) -> Result<String> {
     let mut hasher = blake3::Hasher::new();
-    serde_json::to_writer(hasher.by_ref(), &data)?;
+    hasher.update(&data.salt);
+    hasher.update(&data.nonce);
+    hasher.update(&data.data);
     let hash = hasher.finalize().to_hex().to_string();
     Ok(hash)
 }

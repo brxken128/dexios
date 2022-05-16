1b8111b5fd5c16814aa34b31059728c340b0e1ee
brxken128 <77554505+brxken128@users.noreply.github.com>
2022-05-16 14:26:36 +0100

    6.3.7 - change clap to read cargo.toml version

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ mod structs;
 
 fn main() -> Result<()> {
     let matches = Command::new("dexios")
-        .version("6.3.6")
+        .version(clap::crate_version!())
         .author("brxken128 <github.com/brxken128>")
         .about("Secure command-line encryption of files.")
         .subcommand_required(true)

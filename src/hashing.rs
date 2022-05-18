1dc84c4ee344a4798b9fcc2cf93eb7260520f44e
brxken128 <77554505+brxken128@users.noreply.github.com>
2022-05-18 12:50:48 +0100

    include salt and nonce in checksum

--- src/hashing.rs
+++ src/hashing.rs
@@ -1,3 +1,5 @@
+use std::{fs::File, io::Seek};
+
 use anyhow::{Ok, Result};
 
 use crate::global::DexiosFile;
@@ -9,4 +11,14 @@ pub fn hash_data_blake3(data: &DexiosFile) -> Result<String> {
     hasher.update(&data.data);
     let hash = hasher.finalize().to_hex().to_string();
     Ok(hash)
+}
+
+pub fn hash_data_blake3_stream(file: &mut File) -> Result<String> {
+    let mut hasher = blake3::Hasher::new();
+
+
+
+
+    file.seek(std::io::SeekFrom::Start(0))?;
+    todo!()
 }

0aab51ec11837752a5d43c2aeeb83d486526e586
brxken128 <77554505+brxken128@users.noreply.github.com>
2022-05-19 11:46:00 +0100

    add print specifying # of passes

--- src/erase.rs
+++ src/erase.rs
@@ -16,6 +16,8 @@ pub fn secure_erase(input: &str, passes: i32) -> Result<()> {
     let file = File::create(input).with_context(|| format!("Unable to open file: {}", input))?;
     let mut writer = BufWriter::new(file);
 
+    println!("Erasing {} with {} passes (this may take a while)", input, passes);
+
     for _ in 0..passes {
         // generate enough random bytes in accordance to data's size
         for _ in 0..data.len() / 128 {

01edec7170489bb9fcf42d65680d8fbc1317b533
brxken128 <77554505+brxken128@users.noreply.github.com>
2022-05-19 16:53:53 +0100

    change incorrect key wording

--- src/decrypt/crypto.rs
+++ src/decrypt/crypto.rs
@@ -55,7 +55,7 @@ pub fn decrypt_bytes(
 
     if decrypted_bytes.is_err() {
         return Err(anyhow!(
-            "Unable to decrypt the data. Maybe it's a wrong key, or it's not an encrypted file."
+            "Unable to decrypt the data. Maybe it's the wrong key, or it's not an encrypted file."
         ));
     }
 
@@ -103,7 +103,7 @@ pub fn decrypt_bytes_stream(
             let decrypted_data = stream.decrypt_next(buffer.as_slice());
 
             if decrypted_data.is_err() {
-                return Err(anyhow!("Unable to decrypt the data. Maybe it's a wrong key, or it's not an encrypted file."));
+                return Err(anyhow!("Unable to decrypt the data. Maybe it's the wrong key, or it's not an encrypted file."));
             }
 
             let decrypted_data = decrypted_data.unwrap();
@@ -120,7 +120,7 @@ pub fn decrypt_bytes_stream(
             let decrypted_data = stream.decrypt_last(&buffer[..read_count]);
 
             if decrypted_data.is_err() {
-                return Err(anyhow!("Unable to decrypt the final block of data. Maybe it's a wrong key, or it's not an encrypted file."));
+                return Err(anyhow!("Unable to decrypt the final block of data. Maybe it's the wrong key, or it's not an encrypted file."));
             }
 
             let decrypted_data = decrypted_data.unwrap();

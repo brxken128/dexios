441507e22c5bbc48143abea557ccbe2e82758f2f
brxken128 <77554505+brxken128@users.noreply.github.com>
2022-05-23 10:05:15 +0100

    implement reading/writing from source to zip

--- src/directory.rs
+++ src/directory.rs
@@ -1,9 +1,9 @@
-use std::{path::Path, fs::File};
+use std::{path::Path, fs::File, io::{Read, Write}};
 
 use anyhow::{Result, Context};
 use zip::write::FileOptions;
 
-use crate::{global::{Parameters, DirectoryMode}, file::get_paths_in_dir};
+use crate::{global::{Parameters, DirectoryMode, BLOCK_SIZE}, file::get_paths_in_dir};
 
 pub fn encrypt_directory(input: &str, output: &str, exclude: Vec<&str>, keyfile: &str, mode: DirectoryMode, params: Parameters) -> Result<()> {
     let (files, dirs) = get_paths_in_dir(input, mode)?;
@@ -22,7 +22,31 @@ pub fn encrypt_directory(input: &str, output: &str, exclude: Vec<&str>, keyfile:
         }
     }
 
+    for file in files {
+        zip.start_file(file.to_str().unwrap(), options)?;
 
+        let zip_writer = zip.by_ref();
+        let mut file_reader = File::open(file)?;
+        let file_size = file_reader.metadata().unwrap().len();
+
+        if file_size <= BLOCK_SIZE.try_into().unwrap() {
+            let mut data = Vec::new();
+            file_reader.read_to_end(&mut data)?;
+            zip_writer.write_all(&mut data)?;
+        } else {
+            // stream read/write here
+            let mut buffer = [0u8; BLOCK_SIZE];
+
+            loop {
+                let read_count = file_reader.read(&mut buffer)?;
+                zip_writer
+                    .write_all(&buffer[..read_count])
+                    .context("Unable to write to the output file")?;
+            }
+        }
+    }
+
+    zip.finish()?;
 
     Ok(())
 }

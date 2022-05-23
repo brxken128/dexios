71083b1d6148cf228c48f88709cbd8bb36ec45d7
brxken128 <77554505+brxken128@users.noreply.github.com>
2022-05-23 11:43:47 +0100

    remove useless result

--- src/directory.rs
+++ src/directory.rs
@@ -72,15 +72,13 @@ pub fn encrypt_directory(
     }
     zip.finish()?;
 
-    let result = if memory {
-        crate::encrypt::memory_mode(&tmp_name, output, keyfile, &params)
+    if memory {
+        crate::encrypt::memory_mode(&tmp_name, output, keyfile, &params)?;
     } else {
-        crate::encrypt::stream_mode(&tmp_name, output, keyfile, &params)
+        crate::encrypt::stream_mode(&tmp_name, output, keyfile, &params)?;
     };
 
-    if result.is_ok() {
-        crate::erase::secure_erase(&tmp_name, 16)?;
-    }
+    crate::erase::secure_erase(&tmp_name, 16)?;
 
     Ok(())
 }

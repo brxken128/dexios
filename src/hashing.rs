b43925e670a047b063f65fab1c5fe1d4a430e18b
brxken128 <77554505+brxken128@users.noreply.github.com>
2022-05-25 17:54:30 +0100

    update comment about hash-memory mode

--- src/hashing.rs
+++ src/hashing.rs
@@ -34,7 +34,7 @@ pub fn hash_stream(input: &str) -> Result<()> {
 
 // this hashes the input file
 // it reads the file all at once, hashes it and displays the hash
-// it's used by hash-standalone mode
+// it's used by hash-standalone mode when the input file isn't large enough for streaming mode
 pub fn hash_memory(input: &str) -> Result<()> {
     let mut input_file =
         std::fs::File::open(input).with_context(|| format!("Unable to open file: {}", input))?;

2c7868dd6fd360d486d7c41352b2e663f596e7bd
brxken128 <77554505+brxken128@users.noreply.github.com>
2022-06-01 21:10:54 +0100

    bump headerver to v3

--- src/global.rs
+++ src/global.rs
@@ -4,7 +4,7 @@ use self::enums::{Algorithm, HeaderVersion};
 // these can be customised easily by anyone to suit their own needs
 pub const BLOCK_SIZE: usize = 1_048_576; // 1024*1024 bytes
 pub const SALT_LEN: usize = 16; // bytes
-pub const VERSION: HeaderVersion = HeaderVersion::V2;
+pub const VERSION: HeaderVersion = HeaderVersion::V3;
 
 pub const ALGORITHMS: [Algorithm; 3] = [
     Algorithm::XChaCha20Poly1305,

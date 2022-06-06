f7adccac7c86d4961a5e5161474622f962df5cc4
brxken128 <77554505+brxken128@users.noreply.github.com>
2022-06-06 18:07:34 +0100

    comment update

--- src/subcommands/prompt.rs
+++ src/subcommands/prompt.rs
@@ -46,7 +46,7 @@ pub fn get_answer(prompt: &str, default: bool, skip: bool) -> Result<bool> {
 // this checks if the file exists
 // then it prompts the user if they'd like to overwrite a file (while showing the associated file name)
 // if they have the skip argument supplied, this will just assume true
-// if benchmarking or skip are true, skip the prompt entirely
+// if skip mode is true, avoid prompts at all
 pub fn overwrite_check(name: &str, skip: SkipMode) -> Result<bool> {
     let answer = if std::fs::metadata(name).is_ok() {
         let prompt = format!("{} already exists, would you like to overwrite?", name);

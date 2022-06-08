56c17df95cbefb14c0bbdbec0f544e27b07fdb3e
brxken128 <77554505+brxken128@users.noreply.github.com>
2022-06-08 18:51:13 +0100

    add docs for `primitives.rs`

--- src/primitives.rs
+++ src/primitives.rs
@@ -1,13 +1,13 @@
-// this file contains all of the AEAD cryptographic enums/impls
-// you can add other ciphers to the mix mainly in here, if you so wish
-// they just need to be part of the RustCrypto "family"
-// https://github.com/RustCrypto/AEADs
+//! This module contains all cryptographic primitives used by `dexios-core`
 
-// this file sets constants that are used throughout the codebase
-// these can be customised easily by anyone to suit their own needs
+/// This is the streaming block size
+/// NOTE: Stream mode can be used to encrypt files less than this size, provided the implementation is correct
 pub const BLOCK_SIZE: usize = 1_048_576; // 1024*1024 bytes
+
+/// This is the length of the salt used for `argon2id` hashing
 pub const SALT_LEN: usize = 16; // bytes
 
+/// This is an `enum` containing all AEADs supported by `dexios-core`
 #[derive(Copy, Clone)]
 pub enum Algorithm {
     Aes256Gcm,
@@ -15,7 +15,9 @@ pub enum Algorithm {
     DeoxysII256,
 }
 
-pub const ALGORITHMS: [Algorithm; 3] = [
+/// This is an array containing all AEADs supported by `dexios-core`.
+/// It can be used by and end-user application to show a list of AEADs that they may use
+pub static ALGORITHMS: [Algorithm; 3] = [
     Algorithm::XChaCha20Poly1305,
     Algorithm::Aes256Gcm,
     Algorithm::DeoxysII256,
@@ -23,7 +25,7 @@ pub const ALGORITHMS: [Algorithm; 3] = [
 
 impl std::fmt::Display for Algorithm {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match *self {
+        match self {
             Algorithm::Aes256Gcm => write!(f, "AES-256-GCM"),
             Algorithm::XChaCha20Poly1305 => write!(f, "XChaCha20-Poly1305"),
             Algorithm::DeoxysII256 => write!(f, "Deoxys-II-256"),
@@ -31,6 +33,7 @@ impl std::fmt::Display for Algorithm {
     }
 }
 
+/// This defines the possible modes used for encrypting/decrypting
 #[derive(PartialEq, Eq)]
 pub enum CipherMode {
     // could do with a better name

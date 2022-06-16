863b5ec43deed1d738185508f53e61ac9a6dcd2e
brxken128 <77554505+brxken128@users.noreply.github.com>
2022-06-16 15:49:54 +0100

    lower t_cost to 1

--- src/key.rs
+++ src/key.rs
@@ -125,7 +125,7 @@ pub fn balloon_hash(
         }
         HeaderVersion::V4 => {
             // change this to v4
-            let params = balloon_hash::Params::new(2097152, 6, 1);
+            let params = balloon_hash::Params::new(2097152, 1, 1);
             match params {
                 Ok(parameters) => parameters,
                 Err(_) => {

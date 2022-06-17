c73ce98697c6af9a514c68845eb6da19f82df1e5
brxken128 <77554505+brxken128@users.noreply.github.com>
2022-06-17 19:22:38 +0100

    add support for using said keyfiles

--- src/subcommands/header.rs
+++ src/subcommands/header.rs
@@ -5,38 +5,39 @@ use std::{
 };
 
 use std::io::Cursor;
-
 use anyhow::{Context, Result};
-use paris::{info, success, Logger};
-
+use paris::{success, Logger};
 use dexios_core::header::{Header, HeaderVersion};
-
-use crate::global::states::SkipMode;
-
+use crate::global::states::{SkipMode, KeyFile};
 use super::prompt::{get_answer, overwrite_check};
+use super::key::get_secret;
+use crate::global::states::PasswordMode;
+use dexios_core::cipher::Ciphers;
+use dexios_core::primitives::Mode;
+use dexios_core::protected::Protected;
+use dexios_core::Zeroize;
+use dexios_core::{key::balloon_hash, primitives::gen_nonce};
+use std::time::Instant;
+use paris::info;
 
 // it takes the path to the header file
-pub fn update_key(input: &str) -> Result<()> {
-    use super::key::get_secret;
-    use crate::global::states::{KeyFile, PasswordMode};
-    use dexios_core::cipher::Ciphers;
-    use dexios_core::primitives::Mode;
-    use dexios_core::protected::Protected;
-    use dexios_core::Zeroize;
-    use dexios_core::{key::balloon_hash, primitives::gen_nonce};
-    use std::time::Instant;
-
-    info!("Getting old key!");
+pub fn update_key(input: &str, keyfile_old: KeyFile, keyfile_new: KeyFile) -> Result<()> {
+    if !keyfile_old.is_present() {
+        info!("Please enter your old password below");
+    }
     let raw_key_old = get_secret(
-        &KeyFile::None,
+        &keyfile_old,
         false,
-        PasswordMode::ForceUserProvidedPassword,
+        PasswordMode::NormalKeySourcePriority,
     )?;
-    info!("Getting new key!");
+
+    if !keyfile_new.is_present() {
+        info!("Please enter your new password below");
+    }
     let raw_key_new = get_secret(
-        &KeyFile::None,
+        &keyfile_new,
         true,
-        PasswordMode::ForceUserProvidedPassword,
+        PasswordMode::NormalKeySourcePriority,
     )?;
 
     let mut input_file = OpenOptions::new()

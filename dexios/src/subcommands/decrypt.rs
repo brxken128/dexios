b89cb9be895a084c995cf2dd05af9ef8fc185bf5
brxken128 <77554505+brxken128@users.noreply.github.com>
2022-07-09 19:17:49 +0100

    cargo fmt

--- dexios/src/subcommands/decrypt.rs
+++ dexios/src/subcommands/decrypt.rs
@@ -79,11 +79,7 @@ pub fn memory_mode(input: &str, output: &str, params: &CryptoParams) -> Result<(
     let mut output_file = File::create(output).context("Unable to create output file")?;
 
     let hash_start_time = Instant::now();
-    let key = argon2id_hash(
-        raw_key,
-        &header.salt.unwrap(),
-        &header.header_type.version,
-    )?;
+    let key = argon2id_hash(raw_key, &header.salt.unwrap(), &header.header_type.version)?;
     let hash_duration = hash_start_time.elapsed();
     success!(
         "Successfully hashed your key [took {:.2}s]",
@@ -180,11 +176,7 @@ pub fn stream_mode(input: &str, output: &str, params: &CryptoParams) -> Result<(
     let key = match header.header_type.version {
         HeaderVersion::V1 | HeaderVersion::V2 | HeaderVersion::V3 => {
             let hash_start_time = Instant::now();
-            let key = argon2id_hash(
-                raw_key,
-                &header.salt.unwrap(),
-                &header.header_type.version,
-            )?;
+            let key = argon2id_hash(raw_key, &header.salt.unwrap(), &header.header_type.version)?;
             let hash_duration = hash_start_time.elapsed();
             success!(
                 "Successfully hashed your key [took {:.2}s]",
@@ -196,9 +188,7 @@ pub fn stream_mode(input: &str, output: &str, params: &CryptoParams) -> Result<(
             let hash_start_time = Instant::now();
             let keyslot = header.keyslots.unwrap();
 
-            let key = keyslot[0]
-                .hash_algorithm
-                .hash(raw_key, &keyslot[0].salt)?;
+            let key = keyslot[0].hash_algorithm.hash(raw_key, &keyslot[0].salt)?;
             let hash_duration = hash_start_time.elapsed();
             success!(
                 "Successfully hashed your key [took {:.2}s]",

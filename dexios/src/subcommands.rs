3e03b20c3d848f8284f14d135dfde866336cff49
brxken128 <77554505+brxken128@users.noreply.github.com>
2022-07-19 10:43:33 +0100

    fix comment and remove dead code

--- dexios/src/subcommands.rs
+++ dexios/src/subcommands.rs
@@ -24,7 +24,7 @@ pub fn encrypt(sub_matches: &ArgMatches) -> Result<()> {
     let params = parameter_handler(sub_matches)?;
     let algorithm = encrypt_additional_params(sub_matches)?;
 
-    // stream mode is the only mode to decrypt (v8.5.0+)
+    // stream mode is the only mode to encrypt (v8.5.0+)
     encrypt::stream_mode(
         &get_param("input", sub_matches)?,
         &get_param("output", sub_matches)?,
@@ -35,7 +35,6 @@ pub fn encrypt(sub_matches: &ArgMatches) -> Result<()> {
 
 pub fn decrypt(sub_matches: &ArgMatches) -> Result<()> {
     let params = parameter_handler(sub_matches)?;
-    // let header = decrypt_additional_params(sub_matches)?;
 
     // stream decrypt is the default as it will redirect to memory mode if the header says so (for backwards-compat)
     decrypt::stream_mode(

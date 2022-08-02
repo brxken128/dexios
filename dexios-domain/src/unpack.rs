182cee49a951973d9ee925ff18c503a8dbec1644
Dmitriy Pleshevskiy <dmitriy@ideascup.me>
2022-08-02 14:36:12 +0300

    domain/unpack: extract main logic

---
+++ dexios-domain/src/unpack.rs
@@ -0,0 +1,119 @@
+use std::cell::RefCell;
+use std::io::{Read, Seek, Write};
+use std::path::PathBuf;
+use std::sync::Arc;
+
+use crate::storage::{self, Storage};
+use crate::{decrypt, overwrite};
+use dexios_core::protected::Protected;
+
+#[derive(Debug)]
+pub enum Error {
+    OpenArchive,
+    ResetCursorPosition,
+    Storage(storage::Error),
+    Decrypt(decrypt::Error),
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::OpenArchive => f.write_str("Unable to open archive"),
+            Error::ResetCursorPosition => f.write_str("Unable to reset cursor position"),
+            Error::Storage(inner) => write!(f, "Storage error: {}", inner),
+            Error::Decrypt(inner) => write!(f, "Decrypt error: {}", inner),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
+
+pub struct Request<'a, R>
+where
+    R: Read,
+{
+    pub reader: &'a RefCell<R>,
+    pub header_reader: Option<&'a RefCell<R>>,
+    pub raw_key: Protected<Vec<u8>>,
+    pub on_decrypted_header: Option<decrypt::OnDecryptedHeaderFn>,
+    pub output_dir_path: PathBuf,
+}
+
+pub fn execute<RW: Read + Write + Seek>(
+    stor: Arc<impl Storage<RW>>,
+    req: Request<RW>,
+) -> Result<(), Error> {
+    // 1. Create temp zip archive.
+    let tmp_file = stor.create_temp_file().map_err(Error::Storage)?;
+
+    // 2. Decrypt input file to temp zip archive.
+    decrypt::execute(decrypt::Request {
+        header_reader: req.header_reader,
+        reader: req.reader,
+        writer: tmp_file
+            .try_writer()
+            .expect("We sure that file in write mode"),
+        raw_key: req.raw_key,
+        on_decrypted_header: req.on_decrypted_header,
+    })
+    .map_err(Error::Decrypt)?;
+
+    let buf_capacity = stor.file_len(&tmp_file).map_err(Error::Storage)?;
+
+    // 3. Recover files from temp archive.
+    {
+        let mut reader = tmp_file
+            .try_reader()
+            .expect("We sure that file in read mode")
+            .borrow_mut();
+
+        reader.rewind().map_err(|_| Error::ResetCursorPosition)?;
+
+        let mut archive = zip::ZipArchive::new(&mut *reader).map_err(|_| Error::OpenArchive)?;
+
+        stor.create_dir_all(&req.output_dir_path)
+            .map_err(Error::Storage)?;
+
+        for i in 0..archive.len() {
+            let mut full_path = req.output_dir_path.clone();
+
+            let zip_file = archive.by_index(i).map_err(|_| todo!())?;
+            match zip_file.enclosed_name() {
+                Some(path) => full_path.push(path),
+                None => continue,
+            }
+
+            // TODO(pleshevskiy): check slip prevention
+
+            if zip_file.is_dir() {
+                stor.create_dir_all(full_path).map_err(Error::Storage)?;
+            } else {
+                // TODO(pleshevskiy): handle the file's existence
+                stor.create_file(full_path).map_err(Error::Storage)?;
+            }
+        }
+    }
+
+    // 4. Finally eraze temp zip archive with zeros.
+    overwrite::execute(crate::overwrite::Request {
+        buf_capacity,
+        writer: tmp_file
+            .try_writer()
+            .expect("We sure that file in write mode"),
+        passes: 2,
+    })
+    .ok();
+
+    stor.remove_file(tmp_file).ok();
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    #[ignore = "not yet implemented"]
+    fn should_unpack_encrypted_archive() {
+        todo!()
+    }
+}

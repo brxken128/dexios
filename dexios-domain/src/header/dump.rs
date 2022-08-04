2a8860548940476167c46a975bd9b360c47cf7b0
brxken128 <77554505+brxken128@users.noreply.github.com>
2022-08-04 15:17:38 +0100

    domain/header: port header dump

---
+++ dexios-domain/src/header/dump.rs
@@ -0,0 +1,29 @@
+use super::Error;
+use std::cell::RefCell;
+use std::io::{Read, Seek, Write};
+
+use dexios_core::header::Header;
+
+pub struct Request<'a, R, W>
+where
+    R: Read + Seek,
+    W: Write + Seek,
+{
+    pub reader: &'a RefCell<R>,
+    pub writer: &'a RefCell<W>,
+}
+
+pub fn execute<R, W>(req: Request<R, W>) -> Result<(), Error>
+where
+    R: Read + Seek,
+    W: Write + Seek,
+{
+    let (header, _) =
+        Header::deserialize(&mut *req.reader.borrow_mut()).map_err(|_| Error::InvalidFile)?;
+
+    header
+        .write(&mut *req.writer.borrow_mut())
+        .map_err(|_| Error::Write)?;
+
+    Ok(())
+}

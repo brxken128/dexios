fca926c8f00b1d5c43e2eabe1090489b75b4fb5f
brxken128 <77554505+brxken128@users.noreply.github.com>
2022-08-04 16:35:57 +0100

    domain/utils: add hex_encode

--- dexios-domain/src/utils.rs
+++ dexios-domain/src/utils.rs
@@ -30,6 +30,13 @@ mod test {
     }
 }
 
+pub fn hex_encode(bytes: &[u8]) -> String {
+    bytes
+        .iter()
+        .map(|b| format!("{:02x}", b))
+        .collect::<String>()
+}
+
 #[cfg(test)]
 pub use test::gen_master_key;
 #[cfg(test)]

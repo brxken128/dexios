e63aabad2170cacee1492e821d8ad5b4ac2b78bf
brxken128 <77554505+brxken128@users.noreply.github.com>
2022-08-04 15:17:45 +0100

    domain/header: port header strip

---
+++ dexios-domain/src/header/strip.rs
@@ -0,0 +1,40 @@
+use super::Error;
+use std::cell::RefCell;
+use std::io::{Read, Seek, Write};
+
+use dexios_core::header::Header;
+
+pub struct Request<'a, RW>
+where
+    RW: Read + Write + Seek,
+{
+    pub handle: &'a RefCell<RW>,
+}
+
+pub fn execute<RW>(req: Request<RW>) -> Result<(), Error>
+where
+    RW: Read + Write + Seek,
+{
+    let (header, _) =
+        Header::deserialize(&mut *req.handle.borrow_mut()).map_err(|_| Error::InvalidFile)?;
+
+    req.handle
+        .borrow_mut()
+        .rewind()
+        .map_err(|_| Error::Rewind)?;
+
+    let zeroes = vec![
+        0u8;
+        header
+            .get_size()
+            .try_into()
+            .map_err(|_| Error::HeaderSizeParse)?
+    ];
+
+    req.handle
+        .borrow_mut()
+        .write_all(&zeroes)
+        .map_err(|_| Error::Write)?;
+
+    Ok(())
+}

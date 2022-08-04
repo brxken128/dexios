170f4d9ab1f79fb1b9a9ea4b44b713cd59165d8a
brxken128 <77554505+brxken128@users.noreply.github.com>
2022-08-04 16:56:07 +0100

    domain: apply lints

--- dexios-domain/src/header.rs
+++ dexios-domain/src/header.rs
@@ -14,7 +14,7 @@ pub enum Error {
 
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        use Error::*;
+        use Error::{HeaderSizeParse, InvalidFile, Read, Rewind, UnsupportedRestore, Write};
         match self {
             UnsupportedRestore => f.write_str("The provided request is unsupported with this file. It maybe isn't an encrypted file, or it was encrypted in detached mode."),
             InvalidFile => f.write_str("The file does not contain a valid Dexios header."),
